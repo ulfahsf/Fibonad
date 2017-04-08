4b7968733626eae1234de46350909bdb0b287595
Hari Nugroho <harinugroho@users.noreply.github.com>
2017-04-08 11:35:16 +0700

    find total of fibonacy number

--- src/fibonanchi/Fibonanchi.java
+++ src/fibonanchi/Fibonanchi.java
@@ -19,13 +19,22 @@ public class Fibonanchi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x=sc.nextInt();
-        deret(x);
+//         deret(x);
+        total(x);
     }
     private static int fibbonaci(int x){
         if(x>1){
             return fibbonaci(x-1)+fibbonaci(x-2);
         }
         return x;
+    }
+    
+    private static void total(int x){
+        int total = 0;
+        for (int i = 1; i <= x; i++) {
+            total+=fibbonaci(i);
+        }
+        System.out.println(total);
     }
 
     private static void deret(int x){

9c042898b2efdee45c57d0ee79c02016bfb14f07
Ulfah S.F <ulfahsuryanifithri8@gmail.com>
2017-04-08 12:10:44 +0700

    Update Fibonanchi.java

--- src/fibonanchi/Fibonanchi.java
+++ src/fibonanchi/Fibonanchi.java
@@ -1,34 +1,32 @@
 
 package fibonanchi;
-
 import java.util.Scanner;
 
 public class Fibonanchi {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x=sc.nextInt();
-//         deret(x);
-        total(x);
+//         deret(y);
+        total(y);
     }
     
-    private static int fibbonaci(int x){
-        if(x>1){
-            return fibbonaci(x-1)+fibbonaci(x-2);
+    private static int fibbonaci(int y){
+        if(y>1){
+            return fibbonaci(y-1)+fibbonaci(y-2);
         }
-        return x;
+        return y;
     }
     
-    private static void total(int x){
+    private static void total(int y){
         int total = 0;
-        for (int i = 1; i <= x; i++) {
+        for (int i = 1; i <= y; i++) {
             total+=fibbonaci(i);
         }
         System.out.println(total);
     }
 
-    private static void deret(int x){
-        for (int i = 1; i <= x; i++) {
+    private static void deret(int y){
+        for (int i = 1; i <= y; i++) {
             System.out.print(fibbonaci(i)+" ");
         }
     }
